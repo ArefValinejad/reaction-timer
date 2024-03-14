6c40dbb358f597d145296795d1eb5b35efb0de6b
Aerf4242 <137071735+Aerf4242@users.noreply.github.com>
2024-03-14 16:41:08 +0330

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <button class="btn" @click="start" :disabled="isPlaying">Play</button>
+  <results v-if="showResult" :score="score" />
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+</template>
+
+<script>
+import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
+
+export default {
+  name: "App",
+  components: { Block, Results },
+  data() {
+    return {
+      delay: null,
+      isPlaying: false,
+      showResult: false,
+      score: null,
+      rank: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResult = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResult = true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+.btn {
+  background: #bb03df;
+  color: aliceblue;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+.btn[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,31 @@
+<template>
+  <p class="par">Reactoin Time: {{ score }} ms</p>
+  <p class="par">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 370) {
+      this.rank = " عالی بود فوق العاده";
+    } else if (this.score < 550) {
+      this.rank = "هی بد نبود";
+    } else {
+      this.rank = "حلزون کی بودی";
+    }
+  },
+};
+</script>
+
+<style>
+.par {
+  font-size: 1.4em;
+  color: rgb(247, 1, 108);
+}
+</style>
